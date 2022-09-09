4b17151c38b086c6103d1d5ea98139c180a94cf8
Lan <442391656@qq.com>
2022-09-10 00:12:56 +0800

    17

--- Part1/17.cpp
+++ Part1/17.cpp
@@ -4,18 +4,14 @@ using namespace std;
 
 int main(){
     vector<int> g, s;
-    int value;
-
-    while(cin >> value){
-        g.push_back(value);
+    char ch;
+    while((ch = cin.get()) != '\n'){
+        g.push_back(ch - '0');
     }
 
-    while(cin >> value){
-        s.push_back(value);
-    }
-
-    for(int i=0; i<g.size(); i++)
+    for(int i=0; i<g.size(); i++){
         cout << g[i] << " ";
+    }
     cout << endl;
     
     return 0;
